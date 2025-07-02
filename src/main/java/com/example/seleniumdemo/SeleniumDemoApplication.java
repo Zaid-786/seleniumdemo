e9038a511d5e5b1fa2656d85de46149ac24b1b64
Zaid-786 <erzaid786786@gmail.com>
2025-07-02 13:14:25 +0530

    first initial commit

---
+++ src/main/java/com/example/seleniumdemo/SeleniumDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.seleniumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumDemoApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SeleniumDemoApplication.class, args);
+	
+	
+	    System.out.println("welcome to programming world ...");
+	
+	}
+
+}
